45d00bd85b89f4ec7b71aa4f7afa0fe726f198a6
Nicolas <nicolas.starbuck@gmail.com>
2021-11-30 09:04:54 +0100

    Remove old test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 import { TodoComponent } from "./components/TodoComponent";
-// import TestComponent from "./components/TestComponent";
 
 function App() {
   return (
     <div className="App">
-      {/* <TestComponent nice person={{ name: "Nicolas", lastName: "Troger" }} /> */}
       <TodoComponent />
     </div>
   );
